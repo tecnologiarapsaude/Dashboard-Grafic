e96d4241a82801cd4face56239f8cbc0ed792754
Welley <monteirowelley7@gmail.com>
2024-08-27 10:17:17 -0300

    juntando os graficos para fazer uma teablea so

--- dashboard.py
+++ dashboard.py
@@ -43,7 +43,7 @@ def fetch_data():
                     file_content = file_response.text
                     file_buffer = StringIO(file_content)
                     df = pd.read_csv(file_buffer)
-                    df_juntar_arquivos = pd.merge(df, df, on='id')
+                    df_juntar_arquivos = pd.merge(df, df, on='id', how='outer')
 
                     st.write(df.head())
                     st.line_chart(df_juntar_arquivos)
