50a19e454ab20423788f7bb818a24328e5b823b5
ErickPTCosta <erick.lawliet@gmail.com>
2024-08-29 22:11:59 +0000

    Refatorar código para adicionar filtro de data e empresa

--- dashboard.py
+++ dashboard.py
@@ -2,10 +2,9 @@ import pandas as pd
 import requests
 import streamlit as st
 from io import StringIO
-from datetime import datetime
+from datetime import timedelta
 
 def fetch_data():
-    # Trazendo id da empresa via o embed do WeWeb
     ID_empresas = st.experimental_get_query_params().get('id_empresas', [None])[0]
 
     if ID_empresas is None:
@@ -13,7 +12,6 @@ def fetch_data():
         return None
 
     try:
-        # Transformando a lista de empresas de strings para inteiros
         id_empresas_list = [int(id_str) for id_str in ID_empresas.split(',')]
     except ValueError:
         st.error("O parâmetro 'id_empresas' deve ser uma lista de inteiros separados por vírgula.")
@@ -27,21 +25,18 @@ def fetch_data():
         response = requests.post(XANO_API_GET, json=payload)
         st.write(response)
          
-        # Pegando o JSON com as informações
         if response.status_code == 200:
             st.write('Resposta recebida com sucesso')
             data = response.json()
             st.write(f'Esse é o json: {data}')
             
-            # Armazenar os DataFrames
             dataframes = []
 
-            # Fazendo o loop para pegar todas as empresas no JSON e gerar o gráfico 
             for arquivo in data:
                 arquivo_detalhamento = arquivo['arquivo_detalhamento_vidas']
                 arquivo_url = arquivo_detalhamento['url']
                 st.write(f"URL do arquivo: {arquivo_url}")
-                st.write(f'Esse é os dados do arquivo detalhamento: {arquivo_detalhamento}')
+                st.write(f' Esse é os dados do aquivo detalhamento: {arquivo_detalhamento}')
 
                 file_response = requests.get(arquivo_url)
                 if file_response.status_code == 200:
@@ -50,59 +45,46 @@ def fetch_data():
                     file_buffer = StringIO(file_content)
                     df = pd.read_csv(file_buffer)
 
-                    # Adicionando coluna 'created_at' para filtragem
-                    df['created_at'] = datetime.fromtimestamp(arquivo['created_at'] / 1000)
+                    # Adicionar uma coluna de data
+                    df['created_at'] = pd.to_datetime(arquivo['created_at'], unit='ms')
+                    df.set_index('created_at', inplace=True)
+
                     dataframes.append(df)
                 else:
                     st.error(f"Erro ao baixar o arquivo CSV: {file_response.status_code}")
 
             if len(dataframes) != 0:    
-                # Concatenar os DataFrames
-                combined_df = pd.concat(dataframes, ignore_index=True)
+                combined_df = pd.concat(dataframes, ignore_index=False)
                 
                 st.write(combined_df.head())
 
-                # Gerar menu lateral com filtros
                 st.sidebar.header('Filtros')
 
-                # Filtrar por empresas
+                # Filtro por data usando slider
+                data_inicial = combined_df.index.min().to_pydatetime()
+                data_final = combined_df.index.max().to_pydatetime()
+
+                intervalo_datas = st.sidebar.slider('Selecione as datas', 
+                                                    min_value=data_inicial,
+                                                    max_value=data_final, 
+                                                    value=(data_inicial, data_final),
+                                                    step=timedelta(days=1))
+
+                dados_filtrados = combined_df.loc[intervalo_datas[0]:intervalo_datas[1]]
+
+                # Filtro por empresa
                 empresa_selecionada = st.sidebar.multiselect(
                     'Selecione a empresa',
-                    options=combined_df['EMPRESA'].unique(),
-                    default=combined_df['EMPRESA'].unique(),
+                    options=dados_filtrados['EMPRESA'].unique(),
+                    default=dados_filtrados['EMPRESA'].unique(),
                     placeholder='Selecione a Empresa'
                 )
 
-                # Filtro por data de criação
-                min_date = combined_df['created_at'].min().date()
-                max_date = combined_df['created_at'].max().date()
-
-                # Garantir que o retorno seja uma lista de duas datas
-                date_range = st.sidebar.date_input(
-                    'Selecione o intervalo de datas',
-                    [min_date, max_date],
-                    min_value=min_date,
-                    max_value=max_date
-                )
-
-                if isinstance(date_range, list) and len(date_range) == 2:
-                    start_date, end_date = tuple(date_range)
-                else:
-                    st.error("Por favor, selecione um intervalo de datas válido.")
-                    return None
-
-                # Aplicar filtro de data
-                mask = (combined_df['created_at'] >= pd.to_datetime(start_date)) & (combined_df['created_at'] <= pd.to_datetime(end_date))
-                combined_df = combined_df[mask]
-
-                # Aplicar filtro de empresa
                 if empresa_selecionada:
-                    # Filtrar dados com base na seleção da empresa
-                    combined_df = combined_df[combined_df['EMPRESA'].isin(empresa_selecionada)]
-                    # Criar os gráficos com os arquivos
-                    st.line_chart(combined_df)
+                    dados_filtrados = dados_filtrados[dados_filtrados['EMPRESA'].isin(empresa_selecionada)]
+                    st.line_chart(dados_filtrados)
                 else:
-                    st.line_chart(combined_df)                
+                    st.line_chart(dados_filtrados)                
             else:
                 st.error("Menos de dois arquivos CSV foram encontrados.")
 
