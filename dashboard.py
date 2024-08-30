d97d9ff0408ce97cfb17c8c8bb1f9a5f27745169
ErickPTCosta <erick.lawliet@gmail.com>
2024-08-30 21:14:05 +0000

    feat: Update data fetching logic and add sidebar filters

--- dashboard.py
+++ dashboard.py
@@ -3,10 +3,9 @@ import requests
 import streamlit as st
 from io import StringIO
 from datetime import datetime
-import plotly.express as px
 
 def fetch_data():
-    # Trazendo ID da empresa via o embed do WeWeb
+    # Trazendo id da empresa via o embed do WeWeb
     ID_empresas = st.experimental_get_query_params().get('id_empresas', [None])[0]
 
     if ID_empresas is None:
@@ -28,25 +27,49 @@ def fetch_data():
         response = requests.post(XANO_API_GET, json=payload)
         st.write(response)
          
-        # Pegando o JSON com as informações
+        # Pegando o json com as informações
         if response.status_code == 200:
             st.write('Resposta recebida com sucesso')
             data = response.json()
-            st.write(f'Esse é o JSON: {data}')
+            st.write(f'Esse é o json: {data}')
             
-            # Armazenar os DataFrames
+            # Gerar menu lateral com filtros
+            st.sidebar.header('Filtros')
+
+            # Extraindo as datas de criação (created_at) para filtrar
+            created_at_dates = [datetime.fromtimestamp(arquivo['created_at'] / 1000).date() for arquivo in data]
+
+            # Definindo o intervalo de datas para o slider
+            min_date = min(created_at_dates)
+            max_date = max(created_at_dates)
+            selected_date_range = st.sidebar.slider(
+                'Selecione o intervalo de datas',
+                min_value=min_date,
+                max_value=max_date,
+                value=(min_date, max_date),
+                format="YYYY-MM-DD"
+            )
+
+            # Filtrar os dados JSON com base no intervalo de datas selecionado
+            start_date, end_date = selected_date_range
+            filtered_data = [arquivo for arquivo in data if start_date <= datetime.fromtimestamp(arquivo['created_at'] / 1000).date() <= end_date]
+
+            # Se nenhum filtro de data estiver ativo, use todos os dados
+            if len(filtered_data) == 0:
+                filtered_data = data
+
+            # Armazenar os dataframes
             dataframes = []
 
-            # Fazendo o for para pegar todas as empresas que estão no JSON, e gerar o gráfico 
-            for arquivo in data:
-                data_vencimento = arquivo['data_vencimento']
-                data_vencimento_date = pd.to_datetime(data_vencimento)
-                st.write(f'Data de vencimento do arquivo: {data_vencimento_date}')
+            # Fazendo o loop para pegar todas as empresas que estão no JSON e gerar o gráfico 
+            for arquivo in filtered_data:
+                created_at_date = datetime.fromtimestamp(arquivo['created_at'] / 1000)
+                st.write(f'Data de criação do arquivo: {created_at_date}')
 
                 arquivo_detalhamento = arquivo['arquivo_detalhamento_vidas']
                 arquivo_url = arquivo_detalhamento['url']
                 st.write(f"URL do arquivo: {arquivo_url}")
-                st.write(f'Dados do arquivo detalhamento: {arquivo_detalhamento}')
+                st.write(f'Esse é os dados do arquivo detalhamento: {arquivo_detalhamento}')
 
                 file_response = requests.get(arquivo_url)
                 if file_response.status_code == 200:
@@ -55,62 +78,38 @@ def fetch_data():
                     file_buffer = StringIO(file_content)
                     df = pd.read_csv(file_buffer)
 
-                    # Adicionando a data de vencimento ao DataFrame
-                    df['data_vencimento'] = data_vencimento_date
+                    # Adicionando a data de criação ao DataFrame
+                    df['created_at'] = created_at_date
 
                     dataframes.append(df)
                 else:
                     st.error(f"Erro ao baixar o arquivo CSV: {file_response.status_code}")
 
-            if dataframes:     
+            if len(dataframes) != 0:    
                 # Concatenar os DataFrames
                 combined_df = pd.concat(dataframes, ignore_index=True)
-
-                # Remover valores nulos na coluna 'data_vencimento'
-                combined_df.dropna(subset=['data_vencimento'], inplace=True)
-                st.write(combined_df.head(100))
-
-                # Gerar menu lateral com filtros
-                st.sidebar.header('Filtros')
-
-                # Filtro de intervalo de datas
-                min_date = combined_df['data_vencimento'].min().date()
-                max_date = combined_df['data_vencimento'].max().date()
-                selected_date_range = st.sidebar.slider(
-                    'Selecione o intervalo de datas',
-                    min_value=min_date,
-                    max_value=max_date,
-                    value=(min_date, max_date),
-                    format="YYYY-MM-DD"
-                )
-
-                # Filtrar o DataFrame com base no intervalo de datas
-                start_date, end_date = selected_date_range
-                mask = (combined_df['data_vencimento'].dt.date >= start_date) & (combined_df['data_vencimento'].dt.date <= end_date)
-                filtered_df = combined_df.loc[mask]
+                
+                st.write(combined_df.head())
 
                 # Filtrar por empresas
                 empresa_selecionada = st.sidebar.multiselect(
                     'Selecione a empresa',
-                    options=filtered_df['EMPRESA'].unique(),
-                    default=filtered_df['EMPRESA'].unique(),
+                    options=combined_df['EMPRESA'].unique(),
+                    default=combined_df['EMPRESA'].unique(),
                     placeholder='Selecione a Empresa'
                 )
 
                 if empresa_selecionada:
                     # Filtrar dados com base na seleção da empresa
-                    dados_filtrados = filtered_df[filtered_df['EMPRESA'].isin(empresa_selecionada)]
-                    filtered_df = dados_filtrados
+                    dados_filtrados = combined_df[combined_df['EMPRESA'].isin(empresa_selecionada)]
+                    combined_df = dados_filtrados
 
-                # Filtrar as colunas numéricas para evitar o erro de tipos mistos
-                numeric_cols = filtered_df.select_dtypes(include=['float64', 'int64']).columns
-
-                if not numeric_cols.empty:
-                    fig_empresa = px.bar(filtered_df, x='EMPRESA', y=numeric_cols[0], title='Mensalidade por Empresa')
-                    st.plotly_chart(fig_empresa)
+                # Exibir o gráfico com os dados filtrados ou o DataFrame completo se o filtro estiver vazio
+                if not combined_df.empty:
+                    st.line_chart(combined_df)
                 else:
-                    st.error("Nenhuma coluna numérica disponível para renderizar o gráfico.")
-                
+                    st.error("Nenhum dado disponível para exibição no gráfico.")
+
             else:
                 st.error("Menos de dois arquivos CSV foram encontrados.")
 
