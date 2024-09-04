6a66c69319ef4102de49d6b53b3ed5fb16a17c09
Welley <monteirowelley7@gmail.com>
2024-09-04 10:44:29 -0300

    criando o grafico de custo de operadora

--- dashboard.py
+++ dashboard.py
@@ -112,7 +112,7 @@ def fetch_data():
 
                 st.sidebar.header('Filtros')
 
-                fig_empresa = px.bar(filtered_df, x='EMPRESA', y=' COBRADO ', title='Mensalidade por Empresa')
+                fig_empresa = px.bar(filtered_df, x='EMPRESA', y='MENSALIDADE', title='Mensalidade por Empresa')
                 st.plotly_chart(fig_empresa)
 
                 # Graficos de distribuição de vidas
@@ -131,7 +131,7 @@ def fetch_data():
                     st.header('Card3')
 
                 # grafico de custo por operadora
-                custo_operadora = px.bar(filtered_df, x='EMPRESA', y='COBRADO', title='Custo por Operadoras')
+                custo_operadora = px.bar(filtered_df, x='EMPRESA', y=' COBRADO ', title='Custo por Operadoras')
                 st.plotly_chart(custo_operadora)
             
             
