bf88448d93d1a8fe910ae3e5ca319c9db95df115
Welley <monteirowelley7@gmail.com>
2024-09-05 09:43:35 -0300

    tentando gerar graficos

--- dashboard.py
+++ dashboard.py
@@ -144,7 +144,7 @@ def fetch_data():
                 
                 # Grafico de Vidas em cada operadora
 
-                empresa = filtered_df['EMPRESA']
+                empresa = filtered_df['EMPRESA'].filter()
                 st.write(empresa)
                     
                 filtered_df['Total_Vidas'] = filtered_df['TITULAR'].count()
