e1851b546603ac69b557a383581ef4c587c7da3e
Welley <monteirowelley7@gmail.com>
2024-09-05 09:25:27 -0300

    tentando gerar graficos

--- dashboard.py
+++ dashboard.py
@@ -148,7 +148,7 @@ def fetch_data():
                     empresa = filtered_df['EMPRESA']
                     st.write(empresa)
                     
-                    filtered_df['Total_Vidas'] = filtered_df['TITULAR'].count()
+                filtered_df['Total_Vidas'] = filtered_df['TITULAR'].count()
                 
                 vidas_operadoras = px.bar(filtered_df, x='EMPRESA', y='Total_Vidas', title='Vidas por Operadora')
                 st.plotly_chart(vidas_operadoras)
