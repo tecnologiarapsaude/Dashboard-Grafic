0d5adc28b52a609a0d9bcfc57d290348b7d07dd2
Welley <monteirowelley7@gmail.com>
2024-09-05 10:23:51 -0300

    tentando gerar graficos

--- dashboard.py
+++ dashboard.py
@@ -144,7 +144,7 @@ def fetch_data():
                 
                 # Grafico de Vidas em cada operadora
                     
-                filtered_df['Total_Vidas'] = filtered_df.groupby(filtered_df['EMPRESA']).value_counts()
+                filtered_df['Total_Vidas'] = filtered_df.groupby(filtered_df['EMPRESA']).count()
                 
                 
                 vidas_operadoras = px.bar(filtered_df, x='EMPRESA', y='Total_Vidas', title='Vidas por Operadora')
