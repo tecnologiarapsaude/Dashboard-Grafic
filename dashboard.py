c63d2ef998728801f4ff537785394048c473a950
Welley <monteirowelley7@gmail.com>
2024-09-09 11:13:30 -0300

    tentando mudar o grafico de empresa para operadoras

--- dashboard.py
+++ dashboard.py
@@ -191,7 +191,7 @@ def fetch_data():
                 # Combine 'ID' e 'Sexo' em uma nova coluna
                 filtered_df['ID_Sexo'] = filtered_df['ID'].astype(str) + ' - ' + filtered_df['SEXO']
 
-                distribuicao_faixa_sexo = px.bar(filtered_df, x='Nome_Fantasia', y='ID_Sexo', title='Distribuição por Faixa Etária e Sexo')
+                distribuicao_faixa_sexo = px.bar(filtered_df, x='ID_Sexo', y='Nome_Fantasia', title='Distribuição por Faixa Etária e Sexo')
                 st.plotly_chart(distribuicao_faixa_sexo)
                 
                 # Grafico de Vidas em cada operadora
