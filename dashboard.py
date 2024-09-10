e1946e8d351d00c1b55afdd2f05238d3b4a27e1e
Welley <monteirowelley7@gmail.com>
2024-09-10 08:54:06 -0300

    fazendo teste para criar um novo grafico com matplotlib

--- dashboard.py
+++ dashboard.py
@@ -208,11 +208,13 @@ def fetch_data():
                 st.plotly_chart(custo_operadora)
 
                 # grafico de distribuição por faixa etaria e sexo
-                filtered_df['Total_Pessoas'] = filtered_df['TITULAR'].value_counts()
+                filtered_df['Total_Pessoas'] = filtered_df['TITULAR'].count()
 
                 # Combine 'ID' e 'Sexo' em uma nova coluna
                 # filtered_df['ID_Sexo'] = filtered_df['ID'].astype(str) + ' - ' + filtered_df['SEXO']
                 total_idades = filtered_df['ID'].value_counts().sort_index()
+
+                st.write(total_idades)
 
                 distribuicao_faixa_sexo = px.bar(filtered_df, x='ID', y='Total_Pessoas', title='Distribuição por Faixa Etária e Sexo')
                 st.plotly_chart(distribuicao_faixa_sexo)
