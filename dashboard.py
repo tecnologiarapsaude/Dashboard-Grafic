3e8eb0d75617ef962e52110f87bf56cb6890fcc6
Welley <monteirowelley7@gmail.com>
2024-09-10 09:19:41 -0300

    fazendo teste para criar um novo grafico com matplotlib

--- dashboard.py
+++ dashboard.py
@@ -224,7 +224,6 @@ def fetch_data():
                 plt.title('Distribuição por Faixa Etária')
                 plt.grid(True, linestyle='--', alpha=0.7)
                 plt.tight_layout()
-                plt.style.use('dark_background')
                 st.pyplot()
 
                 distribuicao_faixa_sexo = px.bar(filtered_df, x='ID', y='Total_Pessoas', title='Distribuição por Faixa Etária e Sexo')
