6057a2cb7f1966987a89923baac8a4d617c6d7ca
Welley <monteirowelley7@gmail.com>
2024-09-10 09:10:29 -0300

    fazendo teste para criar um novo grafico com matplotlib

--- dashboard.py
+++ dashboard.py
@@ -217,7 +217,7 @@ def fetch_data():
 
                 st.write(total_idades)
                 # preparar o grafico com matplotlib
-                plt.figure(figsize=(100, 60))
+                plt.figure(figsize=(10, 6))
                 total_idades.plot(kind='bar',  color='skyblue', edgecolor='black')
                 plt.xlabel('ID')
                 plt.ylabel('Total Pessoas')
