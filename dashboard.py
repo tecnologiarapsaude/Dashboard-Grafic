3cf43adb72f4a3cac128540b719331be81e317fe
Welley <monteirowelley7@gmail.com>
2024-09-12 12:15:27 -0300

    fazendo teste para a criação do grafico de distribuição por vidas

--- dashboard.py
+++ dashboard.py
@@ -259,7 +259,7 @@ def fetch_data():
 
                     stages = ["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"]
                     df_mtl = pd.DataFrame(dict(number=[39, 27.4, 20.6, 11, 3], stage=stages))
-                    filtered_df['office'] = 'Montreal'
+                    df_mtl['office'] = 'Montreal'
                     df_toronto = pd.DataFrame(dict(number=[52, 36, 18, 14, 5], stage=stages))
                     df_toronto['office'] = 'Toronto'
                     df = pd.concat([df_mtl, df_toronto], axis=0)
