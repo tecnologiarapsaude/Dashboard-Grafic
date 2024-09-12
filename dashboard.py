923b52eb6c9649d3d8d0e276921f2f77c144b43c
Welley <monteirowelley7@gmail.com>
2024-09-12 12:16:20 -0300

    fazendo teste para a criação do grafico de distribuição por vidas

--- dashboard.py
+++ dashboard.py
@@ -259,11 +259,11 @@ def fetch_data():
 
                     stages = ["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"]
                     df_mtl = pd.DataFrame(dict(number=[39, 27.4, 20.6, 11, 3], stage=stages))
-                    df_mtl['office'] = 'M'
+                    df_mtl['Sexo'] = 'M'
                     df_toronto = pd.DataFrame(dict(number=[52, 36, 18, 14, 5], stage=stages))
-                    df_toronto['office'] = 'T'
+                    df_toronto['Sexo'] = 'F'
                     df = pd.concat([df_mtl, df_toronto], axis=0)
-                    fig = px.funnel(df, x='number', y='stage', color='office')
+                    fig = px.funnel(df, x='number', y='stage', color='Sexo')
                     st.plotly_chart(fig)
 
 
