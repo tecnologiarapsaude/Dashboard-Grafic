2e31b4f3f566e9c97b12117c175ce58f6efe747d
Welley <monteirowelley7@gmail.com>
2024-09-13 10:37:13 -0300

    testando o grafico de faixa etaria

--- dashboard.py
+++ dashboard.py
@@ -257,6 +257,10 @@ def fetch_data():
                     stages = ["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"]
                     df_mtl = pd.DataFrame(dict(number=[39, 27.4, 20.6, 11, 3], stage=stages))
                     df_mtl['office'] = 'Montreal'
+
+                    st.write(df_mtl)
+
+
                     df_toronto = pd.DataFrame(dict(number=[52, 36, 18, 14, 5], stage=stages))
                     df_toronto['office'] = 'Toronto'
                     df = pd.concat([df_mtl, df_toronto], axis=0)
