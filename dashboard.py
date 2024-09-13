8f3958860a176b9d475c58f72305d230d89fa8af
Welley <monteirowelley7@gmail.com>
2024-09-13 16:37:03 -0300

    modificando o grafico de operadoras

--- dashboard.py
+++ dashboard.py
@@ -227,7 +227,7 @@ def fetch_data():
 
                     filtered_df[' COBRADO '] = pd.to_numeric(filtered_df[' COBRADO '], errors='coerce')
                     # df_total_valor= filtered_df[' COBRADO '].sum()
-                    df_total_valor = filtered_df.groupby(['Nome_Fantasia', ' COBRDO ']).size().reset_index()
+                    df_total_valor = filtered_df.groupby(['Nome_Fantasia', ' COBRADO ']).size().reset_index()
 
                     st.write(df_total_valor)
                     st.write(filtered_df[' COBRADO '].dtype)
@@ -235,7 +235,7 @@ def fetch_data():
                     custo_operadora = px.bar(
                         filtered_df, 
                         x='Nome_Fantasia', 
-                        y=df_total_valor, 
+                        y=' COBRADO ', 
                         title='Custo por Operadoras',
                         labels={'Nome_Fantasia':'Operadora',' COBRADO ':'Valor Cobrado'},
                         color='Nome_Fantasia',  # Adiciona uma cor baseada na contagem
