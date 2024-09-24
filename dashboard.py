8e642168867240e24ad59303bff7398b86d9e260
Welley <monteirowelley7@gmail.com>
2024-09-24 10:10:43 -0300

    tentando concatenar os dois dataframes

--- dashboard.py
+++ dashboard.py
@@ -129,7 +129,7 @@ def fetch_data():
 
             if dataframes:
                 # Concatenar os DataFrames
-                combined_df = pd.concat([df, df_cnu])
+                combined_df = pd.concat([df])
                 combined_df = combined_df.dropna()
                 st.write(combined_df.head(100))
 
